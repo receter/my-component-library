f6a8ae785cf81354f90d5f2672e9a847273e202c
Andreas Riedmüller <andreas@simplease.at>
2023-08-09 17:10:47 +0200

    installed and configured vite-plugin-lib-inject-css

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     dts({ include: ['lib'] })
   ],
   build: {
