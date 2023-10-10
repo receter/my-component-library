b4863ca8fd6f3689cd137244725c4c94bfc22921
Andreas Riedmüller <receter@gmail.com>
2023-10-10 14:45:11 +0200

    removed css injection

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,11 @@ import { fileURLToPath } from 'node:url'
 import { glob } from 'glob'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    libInjectCss(),
     dts({ include: ['lib'] })
   ],
   build: {
